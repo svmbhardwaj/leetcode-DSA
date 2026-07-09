0f7cca108f249b369c3a199155eea76f1de10b67
Shivam Bhardwaj <svmbhardwaj0@gmail.com>
2026-07-09 21:10:21 +0530

    Time: 0 ms (100%), Space: 7.9 MB (29.92%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     bool divisorGame(int n) {
-        if(n%2 == 0){
-            return true;
-        }else{
-            return false;
-        }
+        return n%2 == 0;
     }
 };
