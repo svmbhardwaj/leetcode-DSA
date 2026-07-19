607dda80823cbd81e8ad015faa23bd347d826f02
Shivam Bhardwaj <svmbhardwaj0@gmail.com>
2026-07-19 23:36:30 +0530

    Time: 0 ms (100%), Space: 8 MB (80.51%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int cnt = 0;
+        if (word.size() == 1) return true;
+
+        for (char c : word)
+            if (isupper(c)) cnt++;
+        if (cnt == 1 && isupper(word[0])) return true;
+        if (cnt == 0 || cnt == word.size()) return true;
+
+        return false;
+    }
+};
